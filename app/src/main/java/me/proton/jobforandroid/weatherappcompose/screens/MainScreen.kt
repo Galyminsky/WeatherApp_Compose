1509d5f14264529be92d16af0320936c48278a26
Galyaminsky_Linux <jobforandroid@proton.me>
2023-10-06 15:22:58 +0600

    # 3 Add dependency

--- app/src/main/java/me/proton/jobforandroid/weatherappcompose/screens/MainScreen.kt
+++ app/src/main/java/me/proton/jobforandroid/weatherappcompose/screens/MainScreen.kt
@@ -2,13 +2,18 @@ package me.proton.jobforandroid.weatherappcompose.screens
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.layout.ContentScale
@@ -16,10 +21,14 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
 import me.proton.jobforandroid.weatherappcompose.R
 import me.proton.jobforandroid.weatherappcompose.ui.theme.Purple40
+import me.proton.jobforandroid.weatherappcompose.ui.theme.Purple80
 
-@Preview (showBackground = true)
+@Preview(showBackground = true)
 @Composable
 fun MainScreen() {
     Image(
@@ -31,18 +40,40 @@ fun MainScreen() {
         contentScale = ContentScale.Crop
     )
 
-    Column (
+    Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(5.dp),
 
-    ){
-    Card(
-        modifier = Modifier.fillMaxWidth().background(Purple40),
-        shape = RoundedCornerShape(10.dp)
+        ) {
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Purple80),
+            shape = RoundedCornerShape(10.dp)
 
-    ) {
-
-    }
+        ) {
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(
+                        modifier = Modifier.padding(top = 8.dp, start = 8.dp),
+                        text = "20 October 2023 13:00",
+                        style = TextStyle(fontSize = 13.sp),
+                        color = Color.Black
+                    )
+                    AsyncImage(
+                        model = "https://cdn.weatherapi.com/weather/64x64/day/116.png",
+                        contentDescription = "img_download",
+                        modifier = Modifier.size(35.dp).padding(top = 3.dp, end = 8.dp)
+                        )
+                }
+            }
+        }
     }
 }
