d85bd6b966fafa6aeee7ff8e833e74e7726a87df
Galyaminsky_Linux <jobforandroid@proton.me>
2023-10-06 14:34:15 +0600

    # 2 Add Card

--- app/src/main/java/me/proton/jobforandroid/weatherappcompose/MainActivity.kt
+++ app/src/main/java/me/proton/jobforandroid/weatherappcompose/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import me.proton.jobforandroid.weatherappcompose.screens.MainScreen
 import me.proton.jobforandroid.weatherappcompose.ui.theme.WeatherAppComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,7 +18,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             WeatherAppComposeTheme {
-
+                MainScreen()
             }
         }
     }
--- app/src/main/java/me/proton/jobforandroid/weatherappcompose/screens/MainScreen.kt
+++ app/src/main/java/me/proton/jobforandroid/weatherappcompose/screens/MainScreen.kt
@@ -1,15 +1,48 @@
 package me.proton.jobforandroid.weatherappcompose.screens
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.graphics.Color
 import me.proton.jobforandroid.weatherappcompose.R
+import me.proton.jobforandroid.weatherappcompose.ui.theme.Purple40
 
 @Preview (showBackground = true)
 @Composable
 fun MainScreen() {
     Image(
-        painter = painterResource(id = R.drawable.oblaka_nebo_zheltyj),
-        contentDescription = "image_back" )
+        painter = painterResource(id = R.drawable.oblako_luna_nebo),
+        contentDescription = "image_back",
+        modifier = Modifier
+            .fillMaxSize()
+            .alpha(0.5f),
+        contentScale = ContentScale.Crop
+    )
+
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(5.dp),
+
+    ){
+    Card(
+        modifier = Modifier.fillMaxWidth().background(Purple40),
+        shape = RoundedCornerShape(10.dp)
+
+    ) {
+
+    }
+    }
 }
